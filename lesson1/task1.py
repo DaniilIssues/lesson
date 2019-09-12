3e60e59126e612ff0be0dd8beac912b4a724f77d
DaniilIssues <47916079+DaniilIssues@users.noreply.github.com>
2019-09-12 14:24:47 +0500

    comleted

---
+++ lesson1/task1.py
@@ -0,0 +1,7 @@
+# Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+# https://drive.google.com/file/d/1pQ-FAfXQy3MeIcjPZ9ORQS528H6fdpSa/view?usp=sharing
+a = input("Введите число: ")
+b, c, d = a[0], a[1], a[2]
+print(f'Произведение чисел: {int(b)*int(c)*int(d)}')
+print(f'Сумма чисел: {int(b)+int(c)+int(d)}')
+
